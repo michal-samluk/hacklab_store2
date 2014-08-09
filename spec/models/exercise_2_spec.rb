fbee6cb6e9662fd3166ca6c14a85b723f1d8c939
Michał Samluk <michal@codequest.eu>
2014-08-09 10:53:54 +0200

    new exercises

--- spec/models/exercise_2_spec.rb
+++ spec/models/exercise_2_spec.rb
@@ -3,6 +3,12 @@ require "rails_helper"
 describe "Exercise 2", :type => :model do
   fixtures :all
 
+  it "Count items that were ordered" do
+    expect(Item.ordered.count).to eq(10)
+  end
 
+  it "Count items that were unordered" do
+    expect(Item.unordered.count).to eq(10)
+  end
 
 end
